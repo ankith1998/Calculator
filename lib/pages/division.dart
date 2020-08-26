4d43f3e107f6ed1acb7a0cb7074e141d8bbc5363
ankith1998 <ajkarthik1315@gmail.com>
2020-08-26 08:39:44 +0530

    Quit button added

--- lib/pages/division.dart
+++ lib/pages/division.dart
@@ -11,71 +11,71 @@ class Div extends StatelessWidget {
         appBar: AppBar(
           title: Text("Division"),
         ),
-        body: SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                SizedBox(height: 20.0,),
-                TextField(
-                  controller: num1,
-                  decoration: InputDecoration(
-                    hintText: "Enter number",
-                    prefixIcon: Icon(Icons.create),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0),
+          body: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  SizedBox(height: 20.0,),
+                  TextField(
+                    controller: num1,
+                    decoration: InputDecoration(
+                      hintText: "Enter number",
+                      prefixIcon: Icon(Icons.create),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(height: 20.0,),
-                TextField(
-                  controller: num2,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.create),
-                    hintText: "Enter number",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0),
+                  SizedBox(height: 20.0,),
+                  TextField(
+                    controller: num2,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.create),
+                      hintText: "Enter number",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(height: 20.0,),
-                GestureDetector(
-                  onTap: (){
-                    var n1= double.parse(num1.text);
-                    var n2= double.parse(num2.text);
-                    var div= n1/n2;
-                    print(div);
-                  },
-                  child: Container(
-                    width: 250.0,
-                    height: 50.0,
-                    child: Center(child: Text("Divide")),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20.0),
-                      gradient: LinearGradient(colors: [Colors.amberAccent,Colors.black26]),
+                  SizedBox(height: 20.0,),
+                  GestureDetector(
+                    onTap: (){
+                      var n1= double.parse(num1.text);
+                      var n2= double.parse(num2.text);
+                      var div= n1/n2;
+                      print(div);
+                    },
+                    child: Container(
+                      width: 250.0,
+                      height: 50.0,
+                      child: Center(child: Text("Divide")),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        gradient: LinearGradient(colors: [Colors.amberAccent,Colors.black26]),
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(height: 20.0,),
-                GestureDetector(
-                  onTap: (){
-                    Navigator.of(context).pop(context);
-                  },
-                  child: Container(
-                    width: 250.0,
-                    height: 50.0,
-                    child: Center(child: Text("Back")),
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(colors: [Colors.amberAccent,Colors.black]),
-                      borderRadius: BorderRadius.circular(20.0),
+                  SizedBox(height: 20.0,),
+                  GestureDetector(
+                    onTap: (){
+                      Navigator.of(context).pop(context);
+                    },
+                    child: Container(
+                      width: 250.0,
+                      height: 50.0,
+                      child: Center(child: Text("Back")),
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(colors: [Colors.amberAccent,Colors.black]),
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
-      ),
-    );
+      );
   }
 }
--- lib/pages/operation.dart
+++ lib/pages/operation.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:calculator/pages/add.dart';
 import 'package:calculator/pages/division.dart';
 import 'package:calculator/pages/multiply.dart';
@@ -7,73 +9,89 @@ import 'package:flutter/material.dart';
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          SizedBox(height: 40.0,),
-          GestureDetector(
-            onTap: (){
-              Navigator.of(context).push(MaterialPageRoute(builder:(context)=>Add()));
-            },
-            child: Center(
+    return SingleChildScrollView(
+      child: Container(
+        child: Column(
+          children: [
+            SizedBox(height: 40.0,),
+            GestureDetector(
+              onTap: (){
+                Navigator.of(context).push(MaterialPageRoute(builder:(context)=>Add()));
+              },
+              child: Center(
+                child: Container(
+                  width: 110.0,
+                  height: 110.0,
+                  child: Center(child: Text("Addition")),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100.0),
+                    gradient: LinearGradient(colors: [Colors.teal,Colors.black26]),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20.0,),
+            GestureDetector(
+              onTap: (){
+                Navigator.of(context).push(MaterialPageRoute(builder: (context)=>Subtr()));
+              },
               child: Container(
-                width: 250.0,
-                height: 150.0,
-                child: Center(child: Text("Addition")),
+                width: 110.0,
+                height: 110.0,
+                child: Center(child: Text("Subtraction")),
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20.0),
+                  borderRadius: BorderRadius.circular(100.0),
+                  gradient: LinearGradient(colors: [Colors.black26,Colors.teal])
+                ),
+              ),
+            ),
+            SizedBox(height: 20.0,),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => Multiply()));
+              },
+              child: Container(
+                width: 110.0,
+                height: 110.0,
+                child: Center(child: Text("Multipilication")),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100.0),
                   gradient: LinearGradient(colors: [Colors.teal,Colors.black26]),
                 ),
               ),
             ),
-          ),
-          SizedBox(height: 20.0,),
-          GestureDetector(
-            onTap: (){
-              Navigator.of(context).push(MaterialPageRoute(builder: (context)=>Subtr()));
-            },
-            child: Container(
-              width: 250.0,
-              height: 150.0,
-              child: Center(child: Text("Subtraction")),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20.0),
-                gradient: LinearGradient(colors: [Colors.black26,Colors.teal])
+            SizedBox(height: 20.0,),
+            GestureDetector(
+              onTap: (){
+                Navigator.of(context).push(MaterialPageRoute(builder: (context)=>Div()));
+              },
+              child: Container(
+                width: 110.0,
+                height: 110.0,
+                child: Center(child: Text("Division")),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100.0),
+                  gradient: LinearGradient(colors: [Colors.black26, Colors.teal]),
+                ),
               ),
             ),
-          ),
-          SizedBox(height: 20.0,),
-          GestureDetector(
-            onTap: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => Multiply()));
-            },
-            child: Container(
-              width: 250.0,
-              height: 150.0,
-              child: Center(child: Text("Multipilication")),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20.0),
-                gradient: LinearGradient(colors: [Colors.teal,Colors.black26]),
+            SizedBox(height: 20.0,),
+            GestureDetector(
+              onTap: (){exit(0);
+              },
+              child: Container(
+                width: 110.0,
+                height: 110.0,
+                child: Center(child: Text("Quit")),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100.0),
+                  color: Colors.black26,
+                ),
               ),
-            ),
-          ),
-          SizedBox(height: 20.0,),
-          GestureDetector(
-            onTap: (){
-              Navigator.of(context).push(MaterialPageRoute(builder: (context)=>Div()));
-            },
-            child: Container(
-              width: 250.0,
-              height: 150.0,
-              child: Center(child: Text("Division")),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20.0),
-                gradient: LinearGradient(colors: [Colors.black26, Colors.teal]),
-              ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
